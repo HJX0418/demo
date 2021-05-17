bfc0089bb3d026cd4cf04ae63c714a46b518c148
HJX <l>
2021-05-17 16:02:25 +0800

    第一次提交

---
+++ USER/sys/sys.h
@@ -0,0 +1,21 @@
+#ifndef __SYS_H
+#define __SYS_H	 
+
+#include "stm32f10x.h"
+#include "usart.h"
+#include "PWM.h"
+#include "delay.h"
+
+
+#include <string.h> 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void NVIC_Configuration(void);//中断优先级设置	 
+
+
+
+#endif
